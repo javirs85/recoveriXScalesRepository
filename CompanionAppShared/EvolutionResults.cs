0ef96068d573674855192e9ea1d0418411f1314e
JaviRS <javirs@gmail.com>
2024-01-23 12:15:46 +0100

    bugfixing in plotting

---
+++ CompanionAppShared/EvolutionResults.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompanionAppShared;
+
+public class EvolutionResults
+{
+    public int InitialPreVale = 0;
+    public int LastPreValue = 0;
+    public int AveragedPreValue = 0;
+    public bool HasNonPreValues = false;
+    public int FinalValue = 0;
+    public int Increase
+    {
+        get
+        {
+            if (HasNonPreValues) return FinalValue - AveragedPreValue;
+            else return LastPreValue - InitialPreVale;
+        }
+    }
+}
