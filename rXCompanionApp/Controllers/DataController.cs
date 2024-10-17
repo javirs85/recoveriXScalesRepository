fd4175f89cc8f91a241270379575c10ab6c3b95d
JaviRS <javirs@gmail.com>
2024-10-17 09:43:09 +0200

    upgraded to .net8

---
+++ rXCompanionApp/Controllers/DataController.cs
@@ -0,0 +1,27 @@
+﻿using DataBaseConnector;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace rXCompanionApp.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class DataController: ControllerBase
+{
+	private readonly DataBase _context;
+
+	public DataController(DataBase context) 
+	{
+		_context = context;
+	}
+
+	[HttpGet("{id}")]
+	public async Task<IActionResult> GetData(int id)
+	{
+		var data = "{result: this is a test for "+id.ToString()+"}";
+		if (data == null)
+			return NotFound();
+
+		return Ok(data);
+	}
+}
